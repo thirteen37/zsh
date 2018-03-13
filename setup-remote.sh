ab695be6c4dd13e8d06a2324796a0001ab923f29
Yu-Xi Lim <limyuxi@gmail.com>
2018-03-13 10:27:22 +0800

    Handle case when remote can't clone .zgen

--- setup-remote.sh
+++ setup-remote.sh
@@ -9,8 +9,14 @@ rsync -pav ${SCRIPT_DIR} $REMOTE_HOST: --delete
 ssh $REMOTE_HOST <<EOF
 ln -sf oh-my-zgen/.zshrc
 ln -sf oh-my-zgen/.zshenv
-rm -rf .zgen
 EOF
+
+if [ -n $COPY_ZGEN ]; then
+    rsync -pav ~/.zgen $REMOTE_HOST: --delete
+    ssh $REMOTE_HOST "rm -f .zgen/init.zsh"
+else
+    ssh $REMOTE_HOST "rm -rf .zgen"
+fi
 
 # Screen
 ssh $REMOTE_HOST "cat > .screenrc" <<EOF
